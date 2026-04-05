2fcbe3ef09bf1d6a80af95e3058f01b783a8067f
Quoc Khanh <qckhanh205@gmail.com>
2026-04-05 11:35:35 +0700

    3 4/5/26

--- project-3--calculator/script.js
+++ project-3--calculator/script.js
@@ -1,59 +1,72 @@
-let display = document.getElementById('display');
-let current = '';
-let operator = '';
+let display = document.getElementById("display");
+let displayExtra = document.getElementById("display-extra");
+let current = "";
+let operator = "";
 let resetNext = false;
-
-function appendNumber(num){
-	if(resetNext){
-		current = '';
-		resetNext = false;
-	}
-	if(num === '.' && current.includes('.')) return;
-	current += num;
-	display.textContent = current;
+function appendNumber(num) {
+  if (resetNext) {
+    current = "";
+    resetNext = false;
+    displayExtra.style.visibility = "hidden";
+  }
+  if (num === "." && current.includes(".")) return;
+  current += num;
+  display.textContent = current;
 }
-
-function appendOperator(op){
-	if(current === '') return;
-	if(operator !== '') calculate();
-	operator = op;
-	current += ' ' + op + ' ';
-	display.textContent = current
-		.replace('*','×')
-		.replace('/','÷');
-}
-
-function calculate(){
-	try {
-		current = eval(current).toString();
+function appendOperator(op) {
+  if (current === "" && op !== "-") return;
+	if(current === "" && op === "-"){
+		current = "-";
 		display.textContent = current;
-		operator = '';
-		resetNext = true;
-	} catch {
-		display.textContent = 'error';
-		current = '';
-		operator = '';
+		return;
 	}
+  if (operator !== "") calculate();
+  operator = op;
+  current += " " + op + " ";
+  display.textContent = current;
 }
-
-function clearDisplay(){
-	current = '';
-	operator = '';
-	display.textContent = '0';
+function clearDisplay() {
+  current = "";
+  operator = "";
+  display.textContent = "0";
+  displayExtra.style.visibility = "hidden";
 }
-
-function toggleSign(){
-	if(!current) return;
-	if(current.startsWith('-')){
-		current = current.slice(1);
-	} else{
-		current = '-' + current;
-	}
-	display.textContent = current;
+function deleteOne() {
+	if (resetNext) return;
+  if (current.length <= 1) {
+    current = "";
+    display.textContent = "0";
+    return;
+  }
+  current = current.slice(0, -1);
+  display.textContent = current;
 }
-
-function percent(){
-	if(current === '') return;
-	current = (parseFloat(current)/100).toString();
-	display.textContent = current;
+function calculate() {
+  try {
+    let expression = current;
+    current = eval(current).toString();
+    display.textContent = current;
+    displayExtra.textContent = expression + " =";
+    displayExtra.style.visibility = "visible";
+    operator = "";
+    resetNext = true;
+  } catch {
+    display.textContent = "Error,try again!";
+    current = "";
+    operator = "";
+  }
+}
+function toggleSign() {
+  if (!current) return;
+  if (current.startsWith("-")) {
+    current = current.slice(1);
+  } else {
+    current = "-" + current;
+  }
+  display.textContent = current;
+}
+function percent() {
+  if (current === "") return;
+  current = (parseFloat(current) / 100).toString();
+  display.textContent = current;
 }
