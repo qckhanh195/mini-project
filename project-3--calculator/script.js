708fe27c73c07541230b93652fb17be4e7c4cc81
Quoc Khanh <qckhanh205@gmail.com>
2026-04-05 10:45:34 +0700

    2 4/5/26

--- project-3--calculator/script.js
+++ project-3--calculator/script.js
@@ -13,3 +13,47 @@ function appendNumber(num){
 	display.textContent = current;
 }
 
+function appendOperator(op){
+	if(current === '') return;
+	if(operator !== '') calculate();
+	operator = op;
+	current += ' ' + op + ' ';
+	display.textContent = current
+		.replace('*','×')
+		.replace('/','÷');
+}
+
+function calculate(){
+	try {
+		current = eval(current).toString();
+		display.textContent = current;
+		operator = '';
+		resetNext = true;
+	} catch {
+		display.textContent = 'error';
+		current = '';
+		operator = '';
+	}
+}
+
+function clearDisplay(){
+	current = '';
+	operator = '';
+	display.textContent = '0';
+}
+
+function toggleSign(){
+	if(!current) return;
+	if(current.startsWith('-')){
+		current = current.slice(1);
+	} else{
+		current = '-' + current;
+	}
+	display.textContent = current;
+}
+
+function percent(){
+	if(current === '') return;
+	current = (parseFloat(current)/100).toString();
+	display.textContent = current;
+}
