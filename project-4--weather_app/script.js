7d1cb6b7836ed0893db64eb477bc942ee8c565d0
Quoc Khanh <qckhanh205@gmail.com>
2026-04-07 17:05:03 +0700

    feat: add api weather app

---
+++ project-4--weather_app/script.js
@@ -0,0 +1,11 @@
+const apiKey = "56b1f30c634df5c89702e1088c82a9e3";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=bangalore";
+
+async function checkWeather() {
+    const response = await fetch(apiUrl + `&appid=${apiKey}`);
+    var data = await response.json();
+
+    console.log(data);
+}
+
+checkWeather();
